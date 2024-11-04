02e1e760ebb86ca6046a62aafe48d954a98a9501
Goutham <gouthamdevaraju@gmail.com>
2024-11-04 17:29:39 +0530

    Adding initial project

---
+++ SwiftUI-OpenWeather/SwiftUI_OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_OpenWeatherApp.swift
+//  SwiftUI-OpenWeather
+//
+//  Created by Goutham Devaraju on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
